54ae4dc7783f73ddbbd6d6490e5e000a069979b7
Yuda iswanto <yudaiswanto95@gmail.com>
2022-10-25 00:04:05 +0700

    commit pertama

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+echo"hello guys <br>";
+$nama = "Yuda Iswanto";
+$bukos= "irma";
+$umury= 17;
+$umurx= 19;
+$selisihumur = $umurx-$umury;
+echo"nama saya $nama nama ibu kos saya $bukos ibu kos saya umurnya masi muda tau, <br> selisihnya cuman $selisihumur tahun.";
+
+
+
+
+?>
